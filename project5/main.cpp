e26da7715802b00b2b57ef36f44dde04fb352720
anders bråte <andebraa@uio.no>
2019-11-22 14:04:10 +0100

    added project 5!

---
+++ project5/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <armadillo>
+#include <mpi.h>
+#include <mpi/mpi.h>
+#define ZERO  1.0E-10
+#include "header.h"
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
